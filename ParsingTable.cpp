85b63cf2e383e71ef93a2e189514ddb099ad8c62
Stratakent <45743399+Stratakent@users.noreply.github.com>
2018-12-09 15:58:30 -0800

    Update ParsingTable.cpp

--- ParsingTable.cpp
+++ ParsingTable.cpp
@@ -358,13 +358,16 @@ bool Language3::parse(string filename) {
 
 				// TODO if we reach an identify check that it isnt a reserved word and that it uses all valid letters and symbols
 
-				/*else if (state == 'a' || state == '+' || state == '-' || state == '*' || state == '/' || state == '(' || state == ')' || state == '$') {
-					check = state;
-					if (check != word[i]) {
-						cout << "Expected a " << check << " at this position.\n";
-						return false;
-					}
-					cout << word[i] << " matches " << check << endl;
+				/*
+				//Brian: modified the check for nonterminals on the table.
+				else if (state == "a" || state == "b" || state == "c" || state == "d" || state == "e" || state == "," || state == ":" || state == ";" || state == "=" || state == "+" || state == "-" || state == "*" || state == "/" || state == "(" || state == ")" || state == "$") {
+				//check the state.
+				check = state;
+				if (state != word[i]) {
+					cout << "Expected a " << check << " at this position.\n";
+					return false; //kick out of the parsing
+				}
+				cout << word[i] << " matches " << check << endl;//table is parsed successfully.
 				}*/
 			} while (check != "word[i]");
 
